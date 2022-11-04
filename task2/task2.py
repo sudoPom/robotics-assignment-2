9def9ba1f04f1b33ba7b325cb2258c43a2695887
sudoPom <sudoPom@github.com>
2022-11-04 17:02:11 +0000

    Forward fixes

--- task2/task2.py
+++ task2/task2.py
@@ -4,24 +4,29 @@ import numpy as np
 class Layer:
 
 	def __init__(self, input_size, output_size):
-		self.node_values = [1000 for _ in range(input_size)]
+		self.node_values = [1 for _ in range(input_size)]
 		self.weights = np.random.normal(0, 1.0, size=(input_size, output_size))
 		self.next_layer = None
+		self.prev_layer = None
 
 	def set_next_layer(self, layer):
 		self.next_layer = layer;
 
 	def forward(self):
 		output_nodes = np.matmul(self.node_values, self.weights)
-		for i in range(len(output_nodes)):
-			output_nodes[i] = self.transition_func(output_nodes[i])
+		self.transition_func(output_nodes)
 		if self.next_layer == None:
 			return output_nodes
 		self.next_layer.node_values = output_nodes
 		return self.next_layer.forward()
 
-	def transition_func(self, val):
-		return 0 if val < 0 else val
+	def transition_func(self, vals):
+		for i in range(len(vals)):
+			vals[i] = 0 if vals[i] < 0 else vals[i]
+		
+
+	def update_weights(self):
+		pass
 	
 
 class NeuralNetwork:
@@ -31,11 +36,20 @@ class NeuralNetwork:
 		for i in range(len(layer_sizes)-1):
 			new_layer = Layer(layer_sizes[i], layer_sizes[i+1])
 			self.layers.append(new_layer)
+		
+		# Set up layer connections
 		for i in range(len(self.layers) - 1):
 			self.layers[i].next_layer = self.layers[i+1]
+		for i in range(len(self.layers)-1, -1, -1):
+			self.layers[i].prev_layer = self.layers[i-1]
 
-	def set_weights 
+	def forward(self):
+		return self.layers[0].forward()
+
+		
+		
 			
 
-nn = NeuralNetwork([2,3,4])
-print(nn.layers[0].forward())
+nn = NeuralNetwork([2,3,4, 100])
+print(nn.forward())
+#print(nn.layers[0].forward())
